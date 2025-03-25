2e5d02d57bb325900b45f3bde15b4ed1929438d8
Aleksey Lapaev <sigrtmax42@gmail.com>
2025-03-25 13:22:38 +0300

    fix: исправит интервал перезапуска

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'yandex-to-google',
             script: 'import-yandex-to-google.js',
-            cron_restart: '* 1 * * *',
+            cron_restart: '*/30 * * * *',
             autorestart: false,
         },
     ],
